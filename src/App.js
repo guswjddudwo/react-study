13065bcecb5950b4b5c0bbaf8728f85bacf77bcf
kimhyunjumg <guswjddudwo@gmail.com>
2024-03-03 14:46:16 +0900

    react base

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import Post from "./post";
+import Test from "./Test";
+import Post from "./Post";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Post />
+      <Test />
+    </div>
+  );
 }
 
 export default App;
+
+/**
+ * 중첩된 컴포넌트 만들기
+ * 4분할된 컴포넌트 만들기
+ * 각각 분할된 화면을 2분할로 다시 컴포넌드 만들기
+ */
--- src/Post.js
+++ src/Post.js
@@ -1,1 +1,9 @@
-export default function Post() {}
+import Test from "./Test";
+import "./Post.css";
+export default function Post() {
+  return (
+    <div className="Post">
+      Post<Test></Test>
+    </div>
+  );
+}
