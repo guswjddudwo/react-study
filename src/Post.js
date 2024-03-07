214919f9f57ad88b2aadc5db1a968f688591b172
kimhyunjumg <guswjddudwo@gmail.com>
2024-03-07 20:17:16 +0900

    연습중

--- src/Post.js
+++ src/Post.js
@@ -1,21 +1,5 @@
-import Test from "./Test";
 import "./Post.css";
-import "./Box1.css";
-import "./Box2.css";
-import "./Box3.css";
-
-import Box1 from "./Box1";
-import Box2 from "./Box2";
-import Box3 from "./Box3";
 
 export default function Post() {
-  return (
-    <div className="Post">
-      Post
-      <Test />
-      <Box1 />
-      <Box2 />
-      <Box3 />
-    </div>
-  );
+  return <div className="Post">Post</div>;
 }
