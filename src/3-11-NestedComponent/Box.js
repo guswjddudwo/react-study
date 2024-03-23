b4fc04a751871cb56717fd3bb708e8f6e6415d41
kimhyunjumg <guswjddudwo@gmail.com>
2024-03-23 19:51:14 +0900

    .

--- src/3-11-NestedComponent/Box.js
+++ src/3-11-NestedComponent/Box.js
@@ -1,4 +1,13 @@
+import InnerBox from "../features/NestedComponent/InnerBox";
 import "./Box.css";
-export default function Box() {
-  return <div className="box">Box1</div>;
+export default function Box(title, bgColor) {
+  return (
+    <div className="box" style={{ backgroundColor: bgcolor }}>
+      <div>{title}</div>
+      <div className="innerBoxwrapper">
+        <InnerBox>InnerBox1</InnerBox>
+        <InnerBox>InnerBox2</InnerBox>
+      </div>
+    </div>
+  );
 }
