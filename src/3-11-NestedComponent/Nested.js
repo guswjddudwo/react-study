adbe4a748d2fbba454c93467d3ce743cd88134fb
kimhyunjumg <guswjddudwo@gmail.com>
2024-03-23 19:50:07 +0900

    .

--- src/3-11-NestedComponent/Nested.js
+++ src/3-11-NestedComponent/Nested.js
@@ -1,12 +1,12 @@
 import "./Nested.css";
 import Box from "./Box";
-export default function Nested() {
+export default function Nested({ children, bgColor }) {
   return (
     <div className="boxWrapper">
-      <Box>1</Box>
-      <Box>2</Box>
-      <Box>3</Box>
-      <Box>4</Box>
+      <Box title="box1" bgColor="red" />
+      <Box title="box2" bgColor="orange" />
+      <Box title="box3" bgColor="yellow" />
+      <Box title="box4" bgColor="green" />
     </div>
   );
 }
