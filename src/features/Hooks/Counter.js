56496e9a8f1baad40902ef4841f54474a720de46
kimhyunjumg <guswjddudwo@gmail.com>
2024-03-30 15:24:38 +0900

    .

--- src/features/Hooks/Counter.js
+++ src/features/Hooks/Counter.js
@@ -18,22 +18,22 @@ export default function Counter() {
     console.log("count", count);
   };
 
-  // [] dependencies 영역에 선언된 state가 변경되면 실행됨
-
-  // 과제
+  // *과제*
   // 카운터를 이용한 계산
   // 숫자 증가는 10까지만 제한 감소는 -10까지만 제한
   // +,- 외에 곱하기 나누기 기능 추가(2로만 곱하거나 나누면 됨)
   // Router 복습하기
 
+  // [] dependencies 영역에 선언된 state가 변경되면 실행됨
+  // useEffect(() => { 변경될 조건 },[ 변경될 값 ]);
+
   useEffect(() => {
     if (count < -10) {
-      setCount(-10); // 0보다 작으면 0으로 설정
+      setCount(-10);
     } else if (count > 10) {
-      setCount(10); // 10보다 크면 10으로 설정
+      setCount(10);
     }
-  }, [count]); // count가 변경될 때마다 실행
-
+  }, [count]);
   return (
     <div className="counter">
       <button onClick={increase}>+</button>
