ff34f80931b32da9f36a304ea862e4f22a02c0d5
kimhyunjumg <guswjddudwo@gmail.com>
2024-08-15 19:23:36 +0900

    수정

--- src/features/NestedComponent/Box.js
+++ src/features/NestedComponent/Box.js
@@ -3,8 +3,8 @@ import InnerBox from "./InnerBox";
 
 // props (properties)
 // 컴넌트가 외부에서 주입 받는 속성을 의미
-// 기본적으로 childern 속성은 가지고 있음
-// <Box>이 부분이 childern</Box>
+// 기본적으로 children 속성은 가지고 있음
+// <Box>이 부분이 children</Box>
 // function Box({props}) {}
 // const props = {children: ... }
 // const
